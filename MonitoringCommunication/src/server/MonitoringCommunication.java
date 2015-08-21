223fe660ce16d7840cbfa4a90a348a5009a97822
Emanuel Krivoy <ekrivoy@gmail.com>
2015-08-21 15:11:10 -0500

    Implemented all MonitoringCommunication functions
    Includes GET ALL, GET "sensor" and GET "sensor,sensor,..." commands

--- MonitoringCommunication/src/server/MonitoringCommunication.java
+++ MonitoringCommunication/src/server/MonitoringCommunication.java
@@ -1,22 +1,30 @@
 package server;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+
+import monitoring.MonitoringController;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import jdk.nashorn.internal.parser.JSONParser;
-import monitoring.MonitoringController;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
 public class MonitoringCommunication extends Thread{
+
+	private final int RESEND_TRIES = 3;
 
 	private int port;
 	private MonitoringController controller;
@@ -28,6 +36,7 @@ public class MonitoringCommunication extends Thread{
 		this.port = port;
 		this.controller = controller;
 		serverSocket = new ServerSocket(port);
+		System.out.println(Base64.encode((getHash(new File("C:\\Users\\Administrador\\Desktop\\hashme.txt")))));
 	}
 
 	@Override
@@ -54,29 +63,41 @@ public class MonitoringCommunication extends Thread{
 					break;
 
 				}
+
 				reader.close();
+				out.close();
+				clientSocket.close();
+
 			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	private void getFile(String services) {
+	private void getFile(String services) throws Exception {
+		PrintWriter writer = new PrintWriter(out);
 
 		if(services.toUpperCase().equals("ALL")) {
-
+			for(File file : controller.getPickupFiles()) {
+				fileProtocol(file, writer);
+			}
 		} else {
 			String[] names = services.split(",");
 			for (String name : names) {
-
+				for(File file : controller.getPickupFiles(name)) {
+					fileProtocol(file, writer);
+				}
 			}
 		}
 
+		writer.close();
 	}
 
 	private void query() {
 		PrintWriter writer = new PrintWriter(out);
-		String[] services = {"hola", "que", "mas"}; //controller.getServicesNames();
+		String[] services = controller.getServicesNames();
 
 		JSONObject response = new JSONObject();
 		response.append("size",services.length);
@@ -89,23 +110,80 @@ public class MonitoringCommunication extends Thread{
 
 	}
 
-	//TODO
-	private List<File> getPickUpFiles() {
+
+	private void fileProtocol(File file, PrintWriter writer) throws Exception{
+		int tries;
+		for (tries = 0; tries < RESEND_TRIES; tries++) {
+
+
+			writer.println("FILE-"+file.getName()+"-"+file.length()+"-Bytes");
+			writer.flush();
+
+			if(!reader.readLine().toUpperCase().startsWith("OK")) {
+				continue;
+			}
+
+			sendFile(file);
+
+			if(!reader.readLine().toUpperCase().startsWith("OK")) {
+				continue;
+			}
+
+			writer.println("HASH-"+Base64.encode(getHash(file)));
+			writer.flush();
+
+			if(!reader.readLine().toUpperCase().startsWith("OK")) {
+				continue;
+			}
+
+			break;
+		}
+
+		if(tries == 3) {
+			writer.println("ERROR-Retries exceeded");
+			writer.flush();
+			throw new Exception("Couldn't send all files");
+		}
+	}
+
+	//TODO buffer size?
+	private void sendFile(File file) throws IOException {
+
+		BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+
+		int count = 0;
+		byte[] buffer = new byte[1024*1024];
+		while ((count = in.read(buffer)) > 0) {
+			out.write(buffer, 0, count);
+			out.flush();
+		}
+
+		in.close();
+	}
+
+	private byte[] getHash(File file) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+
+			int count = 0;
+			byte[] buffer = new byte[1024*1024];
+			while ((count = in.read(buffer)) > 0) {
+				md.update(buffer,0,count);
+			}
+
+			in.close();
+
+			return md.digest();
+
+		} catch (NoSuchAlgorithmException | IOException e) {
+			e.printStackTrace();
+		}
+
 		return null;
-
 	}
 
-	//TODO
-	private List<File> getPickUpFIles(String service) {
-		return null;
-
+	public static void main(String[] args) throws IOException {
+		new MonitoringCommunication(7856, null);
 	}
-
-	//TODO
-	private boolean sendFile(File file) {
-		return false;
-
-	}
-
-
 }
